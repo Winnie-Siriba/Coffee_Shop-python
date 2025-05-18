c1cf96a5ee7b776ab9d4aff13f2505460192e420
Winnie Siriba <winniemoraa234@gmail.com>
2025-05-19 00:09:00 +0300

    creates order class

---
+++ order.py
@@ -0,0 +1,30 @@
+class Order:
+    all_orders = []
+
+    def __init__(self, customer, coffee, price):
+        if not isinstance(customer, Customer):
+            raise TypeError("The customer must be a Customer instance")
+        if not isinstance(coffee, Coffee):
+            raise TypeError("The coffee must be a Coffee instance")
+        if not isinstance(price, float) or not (1.0 <= price <= 10.0):
+            raise ValueError("The price must be a float between 1.0 and 10.0")
+
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
+        Order.all_orders.append(self)
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @property
+    def price(self):
+        return self._price
+
+from customer import Customer
+from coffee import Coffee
